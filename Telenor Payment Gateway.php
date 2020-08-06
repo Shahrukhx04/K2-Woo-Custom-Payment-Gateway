b455ff0b0040d96edaeab53a67a64e1521382e72
shahrukhx04 <shahrukhx04@gmail.com>
2020-08-06 20:14:36 +0500

    Plugin Setup Complete

--- Telenor Payment Gateway.php
+++ Telenor Payment Gateway.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * Plugin Name: WooCommerce Telenor Payment Gateway
+ * Plugin Name: K2-Woo Custom Payment Gateway
  * Plugin URI: https://pookidevs.com
- * Description: Set up billing through Telenor Mobile Carrier.
+ * Description: Add Custom Payment Gateway on Woocommerce Checkout with custom fields.
  * Author: PookiDevs Technologies
  * Author URI: http://pookidevs.com
- * Version: 2.0.0
+ * Version: 1.0.0
  *
 */
 
@@ -13,19 +13,21 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
- 
- 
- add_filter( 'woocommerce_payment_gateways', 'add_custom_gateway_class' );
+
+// Add K2-Woo-Custom Payment Gateway in existing woocommerce gateway methods array
+add_filter( 'woocommerce_payment_gateways', 'add_custom_gateway_class' );
+
 function add_custom_gateway_class( $methods ) {
-    $methods[] = 'WC_Telenor_Gateway';
+    $methods[] = 'WC_K2_Woo_Custom_Gateway_payment';
     return $methods;
 }
 
 
+// Define Custom Gateway Class & Implement Login
 add_action('plugins_loaded', 'init_custom_gateway_class');
 function init_custom_gateway_class(){
 
-    class WC_Telenor_Gateway extends WC_Payment_Gateway {
+    class WC_K2_Woo_Custom_Gateway_payment extends WC_Payment_Gateway {
 
         public $domain;
 
@@ -36,11 +38,11 @@ function init_custom_gateway_class(){
 
             $this->domain = 'custom_payment';
 
-            $this->id                 = 'telenor';
+            $this->id                 = 'K2WooCustomPaymentGateway';
             $this->icon               = apply_filters('woocommerce_custom_gateway_icon', '');
             $this->has_fields         = false;
-            $this->method_title       = __( 'Telenor Payment Gateway', $this->domain );
-            $this->method_description = __( 'Gateway for Telenor Payment Integeration to set up billing through the mobile carrier.', $this->domain );
+            $this->method_title       = __( 'K2-Woo Custom Payment Gateway', $this->domain );
+            $this->method_description = __( 'Description: Add Custom Payment Gateway on Woocommerce Checkout with custom fields.', $this->domain );
 
             // Load the settings.
             $this->init_form_fields();
@@ -52,10 +54,8 @@ function init_custom_gateway_class(){
             $this->order_status = $this->get_option( 'order_status', 'completed' );
 
             // Actions
-            // 			
             
 			add_action( 'wp_enqueue_scripts', array( $this, 'payment_style_scripts' ) );
-
             add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
             add_action( 'woocommerce_thankyou_' . $this->id, array( $this, 'thankyou_page' ) );
 			
@@ -70,18 +70,6 @@ function init_custom_gateway_class(){
 			
 			add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
 
-			// You can also register a webhook here
-
-			//add_filter( 'woocommerce_after_checkout_validation' , 'Validate_Telenor_Fields' );
-
-			// add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) );
-			//ajax for get code
-			add_action('wp_ajax_sf_telenor_getcode', array( $this, 'sf_telenor_getcode' )); // wp_ajax_{action}
-			add_action('wp_ajax_nopriv_sf_telenor_getcode', array( $this, 'sf_telenor_getcode' )); // wp_ajax_nopriv_{action}
-			
-			//get code for verify code
-			add_action('wp_ajax_sf_telenor_verifycode', array( $this, 'sf_telenor_verifycode' )); // wp_ajax_{action}
-			add_action('wp_ajax_nopriv_sf_telenor_verifycode', array( $this, 'sf_telenor_verifycode' )); // wp_ajax_nopriv_{action}
         }
 
         /**
@@ -93,21 +81,21 @@ function init_custom_gateway_class(){
                 'enabled' => array(
                     'title'   => __( 'Enable/Disable', $this->domain ),
                     'type'    => 'checkbox',
-                    'label'   => __( 'Enable Custom Payment', $this->domain ),
+                    'label'   => __( 'Enable K2-Woo Custom Payment Gateway', $this->domain ),
                     'default' => 'yes'
                 ),
                 'title' => array(
                     'title'       => __( 'Title', $this->domain ),
                     'type'        => 'text',
-                    'description' => __( 'This controls the title which the user sees during checkout.', $this->domain ),
-                    'default'     => __( 'Custom Payment', $this->domain ),
+                    'description' => __( 'Enter the Title to show on the checkout page', $this->domain ),
+                    'default'     => __( 'K2-Woo Custom Payment Gateway', $this->domain ),
                     'desc_tip'    => true,
                 ),
                 'order_status' => array(
                     'title'       => __( 'Order Status', $this->domain ),
                     'type'        => 'select',
                     'class'       => 'wc-enhanced-select',
-                    'description' => __( 'Choose whether status you wish after checkout.', $this->domain ),
+                    'description' => __( 'Select Order Status after checkout', $this->domain ),
                     'default'     => 'wc-completed',
                     'desc_tip'    => true,
                     'options'     => wc_get_order_statuses()
@@ -115,31 +103,13 @@ function init_custom_gateway_class(){
                 'description' => array(
                     'title'       => __( 'Description', $this->domain ),
                     'type'        => 'textarea',
-                    'description' => __( 'Payment method description that the customer will see on your checkout.', $this->domain ),
-                    'default'     => __('Payment Information', $this->domain),
+                    'description' => __( 'Enter the Title to show on the checkout page', $this->domain ),
+                    'default'     => __('Checkout with K2-Woo Custom Payment Gateway with additional custom fields', $this->domain),
                     'desc_tip'    => true,
                 )
             );
         }
 
-        /**
-         * Output for the order received page.
-         */
-        public function thankyou_page() {
-          
-        }
-
-        /**
-         * Add content to the WC emails.
-         *
-         * @access public
-         * @param WC_Order $order
-         * @param bool $sent_to_admin
-         * @param bool $plain_text
-         */
-        public function email_instructions( $order, $sent_to_admin, $plain_text = false ) {
-            
-        }
 
         public function payment_fields(){
 
@@ -181,38 +151,9 @@ function init_custom_gateway_class(){
 				return;
 			}
 
-			// no reason to enqueue JavaScript if API keys are not set
-			/*if ( empty( $this->private_key ) || empty( $this->publishable_key ) ) {
-				return;
-			}
-			// do not work with card detailes without SSL unless your website is in a test mode
-			if ( ! $this->testmode && ! is_ssl() ) {
-				return;
-			}*/
-
-			// and this is our custom JS in your plugin directory
-			wp_register_script( 'woocommerce_sf_telenor', plugins_url( 'RecieveButton.js', __FILE__ ), array( 'jquery'), null, false );
-			wp_localize_script( 'woocommerce_sf_telenor', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
-			// in most payment processors you have to use PUBLIC KEY to obtain a token
-			//wp_localize_script( 'woocommerce_misha', 'misha_params', array(
-			//	'publishableKey' => $this->publishable_key
-			//) );
-
-			wp_enqueue_script( 'woocommerce_sf_telenor' );
-
 
 		}
 		
-		public function payment_style_scripts() {
-
-			// we need JavaScript to process a token only on cart/checkout pages, right?
-			if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
-				return;
-			}
-			wp_register_style( 'telenor_css', plugins_url( 'telenor_payment_style.css', __FILE__ ) );
-    		wp_enqueue_style( 'telenor_css' );
-			
-		}
 
         /**
          * Process the payment and return the result.
@@ -227,7 +168,7 @@ function init_custom_gateway_class(){
             $status = 'wc-' === substr( $this->order_status, 0, 3 ) ? substr( $this->order_status, 3 ) : $this->order_status;
 
             // Set order status
-            $order->update_status( $status, __( 'Checkout with Telenor payment Gateway. ', $this->domain ) );
+            $order->update_status( $status, __( 'Checkout with K2-Woo Custom Payment Gateway. ', $this->domain ) );
 
             // Reduce stock levels
             $order->reduce_order_stock();
@@ -252,353 +193,8 @@ add_action('woocommerce_checkout_process', 'process_custom_payment');
 function process_custom_payment(){
 	global $wpdb;
 
-    if($_POST['payment_method'] != 'telenor')
-        return;
-	
-
-    if( !isset($_POST['mobile']) || empty($_POST['mobile']) ) {
-        wc_add_notice( __( 'Please add your Mobile Number' ), 'error' );
-		return;
-	}
-	
-    if( !isset($_POST['transaction']) || empty($_POST['transaction']) ) {
-        wc_add_notice( __( 'Please add your Pin Code' ), 'error' );
-		return;
-	}
-	
-	
-	
-	$Mobile_Number = $_POST['mobile'];
-	$Mobile_code = $_POST['transaction'];
-	
-	//verify mobile number
-	if  (strlen($Mobile_Number) < 11 || !is_numeric($Mobile_Number) ) {
-		wc_add_notice( __( 'Invalid Number' ), 'error' );
-		return;
-	}
-	$code_from_db_result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_codes WHERE msisdn = '{$Mobile_Number}'");
-	if(count($code_from_db_result) < 1){
-		wc_add_notice( __( 'Please generate a code first' ), 'error' );
-		return;
-	}
-	$code_from_db = $code_from_db_result[0]->code;
-	if((string)$code_from_db != (string)$Mobile_code){
-		wc_add_notice( __( "Pin Code incorrect"), 'error' );
-		return;
-	}
-	if(!($code_from_db_result[0]->valid)){
-		wc_add_notice( __( "Invalid code. Please generate a new one."), 'error' );
-		return;
-	}
-	//all was good so mark code as false so it is not used again
-	$wpdb->update(
-		$wpdb->prefix . 'sftelenor_codes', 
-		array( 	
-			'valid' => 0
-		), 
-		array( 'msisdn' => $Mobile_Number ), 
-		array( 
-			'%d'	
-		), 
-		array( '%s' ) 
-	);
-	
-	
-	
-	$cart_amount = (string) WC()->cart->get_cart_total();
-	$cart_amount = substr(preg_replace("/[^0-9]/","", $cart_amount), -4); //This is dangerous!!!
-	$cart_amount = "0.1"; //comment for actual price
-	
-	//get token from database
-	$token_result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'token'");
-	$token = $token_result[0]->value;
-	$token_time_stamp = strtotime($token_result[0]->time_stamp);
-	$time_difference = time() - $token_time_stamp;
-	if ($time_difference > 1800 || empty($token)){ //more than 30 mins or empty
-		//get new token
-		$args = array(
-			'headers' => array(
-			'Authorization' => 'Basic ' . base64_encode( $username . ':' . $password )
-			)
-		);
-		$response =  wp_remote_post( $url, $args);
-		$response = json_decode( wp_remote_retrieve_body( $response ) );
-
-		$token = $response->access_token;
-		//store new token in db
-		if(!empty($token)){
-			$wpdb->update(
-				$wpdb->prefix . 'sftelenor_constants', 
-				array( 
-					'value' => $token,	
-					'time_stamp' => date("Y-m-d H:i:s")
-				), 
-				array( 'name' => 'token' ), 
-				array( 
-					'%s',	// value1
-					'%s'	// value2
-				), 
-				array( '%s' ) 
-			);
-		}
-		else{
-			wc_add_notice( __( "Sorry we are facing a technical problem. Try again later."), 'error' );
-			return;
-		}
-	}
-	//for transactionID
-	$code_from_db_time_stamp = strtotime($code_from_db_result[0]->time_stamp);
-	$code_from_db_time_stamp = date('YmdHis', $code_from_db_time_stamp); 
-
-	$charge_endpoint = 'https://apis.telenor.com.pk/payment/v1/charge';
- 
-	$charge_body = [
-	 		"msisdn" => $Mobile_Number,
-	 		"chargableAmount" => $cart_amount, //change to $cart_amount
-	 		"correlationId" => "{$Mobile_Number}{$Mobile_code}{$code_from_db_time_stamp}", //same as transactionid
-	 		"PartnerID" => "TP-SKILLSFIRST",
-	 		"ProductID" => "SFOneTimeSM-Charge",
-			"TransactionID" => "{$Mobile_Number}{$Mobile_code}{$code_from_db_time_stamp}" //Mobilenumber+code+timestamp(of code)
-	 	];
-
-	$charge_body = wp_json_encode( $charge_body );
-
-	$charge_options = [
-		'body'        => $charge_body,
-		'headers'     => [ 
-			'Content-Type' => 'application/json',
-			'Authorization' => 'Bearer ' . $token,
-		],
-		'timeout'     => 60,
-		'data_format' => 'body',
-	];
-
-	$ChargeApiResponse = wp_remote_post( $charge_endpoint, $charge_options );
-	
-	$Error = json_decode( wp_remote_retrieve_body( $ChargeApiResponse ) );
-
-	
-	if ($Error->errorMessage != null){
-			wc_add_notice( __($Error->errorMessage), 'error' );
-			return;
-	} 
-	//Send text message to user----------------------------------------------------------------
-	$username = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'username'");
-	$username = $username[0]->value;
-
-	//get password from database
-	$password = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'password'");
-	$password = $password[0]->value;
-
-
-
-	//get token from database
-	$url = "https://apis.telenor.com.pk/oauthtoken/v1/generate?grant_type=client_credentials";
-	$token_result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'token'");
-	$token = $token_result[0]->value;
-	$token_time_stamp = strtotime($token_result[0]->time_stamp);
-	$time_difference = time() - $token_time_stamp;
-	if ($time_difference > 1800 || empty($token)){ //more than 30 mins or empty
-		//get new token
-		$args = array(
-			'headers' => array(
-				'Authorization' => 'Basic ' . base64_encode( $username . ':' . $password )
-			)
-		);
-		$response =  wp_remote_post( $url, $args);
-		$response = json_decode( wp_remote_retrieve_body( $response ) );
-
-		$token = $response->access_token;
-		//store new token in db
-		if(!empty($token)){
-			$wpdb->update(
-				$wpdb->prefix . 'sftelenor_constants', 
-				array( 
-					'value' => $token,	
-					'time_stamp' => date("Y-m-d H:i:s")
-				), 
-				array( 'name' => 'token' ), 
-				array( 
-					'%s',	// value1
-					'%s'	// value2
-				), 
-				array( '%s' ) 
-			);
-		}
-	}
-
-
-	$endpoint = 'https://apis.telenor.com.pk/sms/v1/send';
-	$body = [
-		"messageBody" => "Payment has been successful and Rs.{$cart_amount} has been deducted from your account. Thank you for buying this course. Happy Learning!!!",
-		"recipientMsisdn" => $Mobile_Number
-	];
-
-	$body = wp_json_encode( $body );
-
-	$options = [
-		'body'        => $body,
-		'headers'     => [ 
-			'Content-Type' => 'application/json',
-			'Authorization' => 'Bearer ' . $token,
-		],
-		'timeout'     => 60,
-		'data_format' => 'body',
-	];
-
-	$MessageApiResponse = wp_remote_post( $endpoint, $options );
-	
-	
-
-}
-
-/**
- * Update the order meta with field value
- */
-add_action( 'woocommerce_checkout_update_order_meta', 'custom_payment_update_order_meta' );
-function custom_payment_update_order_meta( $order_id ) {
-
-	global $wpdb;
-    //if($_POST['payment_method'] != 'custom')
-     //   return;
-
-	$customer_mobile_number = $_POST['mobile'];
-    if ( ! empty( $customer_mobile_number ) )
-        update_post_meta( $order_id, 'telenor_mobile_number', sanitize_text_field( $customer_mobile_number ) );
-
-    $customer_mobile_code = $_POST['transaction'];
-    if ( ! empty( $customer_mobile_code ) )
-        update_post_meta($order_id, 'code', sanitize_text_field( $customer_mobile_code ) );
-	
-	//getting time_Stamp for this code
-	$code_from_db_result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_codes WHERE msisdn = '{$customer_mobile_number}'");
-
-	$code_from_db_time_stamp = strtotime($code_from_db_result[0]->time_stamp);
-	$code_from_db_time_stamp = date('YmdHis', $code_from_db_time_stamp); 
-    update_post_meta($order_id, 'telenor_transaction_id', "{$customer_mobile_number}{$customer_mobile_code}{$code_from_db_time_stamp}" );
-	
-}
-
-/**
- * Display field value on the order edit page
- */
-add_action( 'woocommerce_admin_order_data_after_billing_address', 'custom_checkout_field_display_admin_order_meta', 10, 1 );
-function custom_checkout_field_display_admin_order_meta($order){
-    $method = get_post_meta( $order->id, '_payment_method', true );
-    if($method != 'custom')
+    if($_POST['payment_method'] != 'K2WooCustomPaymentGateway')
         return;
 
-    $mobile = get_post_meta( $order->id, 'mobile', true );
-    $transaction = get_post_meta( $order->id, 'transaction', true );
-
-    echo '<p><strong>'.__( 'Mobile Number' ).':</strong> ' . $mobile . '</p>';
-    echo '<p><strong>'.__( 'Transaction ID').':</strong> ' . $transaction . '</p>';
 }
 
-
-// register the ajax action for authenticated users
-add_action('wp_ajax_sf_get_code', 'sf_get_code');
-
-// register the ajax action for unauthenticated users
-add_action('wp_ajax_nopriv_sf_get_code', 'sf_get_code');
-
-// handle the ajax request
-function sf_get_code() {
- 
-	global $wpdb;
-	
-	$Mobile_Number = $_REQUEST['mobile_number'];
-	
-	//verify mobile number
-	if  (strlen($Mobile_Number) < 11 || !is_numeric($Mobile_Number) ) {
-		wc_add_notice( __( 'Invalid Number' ), 'error' ); //TODO: only works when page is reloaded
-		return;
-	}
-	
-	$url = "https://apis.telenor.com.pk/oauthtoken/v1/generate?grant_type=client_credentials";
-	
-	//get username form database
-	$username = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'username'");
-	$username = $username[0]->value;
-	
-	//get password from database
-	$password = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'password'");
-	$password = $password[0]->value;
-	
-	
-	
-	//get token from database
-	$token_result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}sftelenor_constants WHERE name = 'token'");
-	$token = $token_result[0]->value;
-	$token_time_stamp = strtotime($token_result[0]->time_stamp);
-	$time_difference = time() - $token_time_stamp;
-	if ($time_difference > 1800 || empty($token)){ //more than 30 mins or empty
-		//get new token
-		$args = array(
-			'headers' => array(
-			'Authorization' => 'Basic ' . base64_encode( $username . ':' . $password )
-			)
-		);
-		$response =  wp_remote_post( $url, $args);
-		$response = json_decode( wp_remote_retrieve_body( $response ) );
-
-		$token = $response->access_token;
-		//store new token in db
-		if(!empty($token)){
-			$wpdb->update(
-				$wpdb->prefix . 'sftelenor_constants', 
-				array( 
-					'value' => $token,	
-					'time_stamp' => date("Y-m-d H:i:s")
-				), 
-				array( 'name' => 'token' ), 
-				array( 
-					'%s',	// value1
-					'%s'	// value2
-				), 
-				array( '%s' ) 
-			);
-		}
-	}
-
-	$Mobile_Code = (string)rand(1000,9999);
-	//store code against number in db
-	$wpdb->query( $wpdb->prepare( 
-		"
-			REPLACE INTO {$wpdb->prefix}sftelenor_codes
-			( msisdn, code, valid, time_stamp )
-			VALUES ( %s, %s, %d, %s )
-		", 
-			$Mobile_Number, 
-			$Mobile_Code, 
-			1,
-			date("Y-m-d H:i:s")
-	) );
-	
-	
-	$endpoint = 'https://apis.telenor.com.pk/sms/v1/send';
-	$customer_name = $_REQUEST['customer_name'];
- 
-	$body = [
-		"messageBody" => "Hi {$customer_name}, This is the message from SkillsFirst Platform. Your code is {$Mobile_Code}",
-		"recipientMsisdn" => $Mobile_Number
-	];
-
-	$body = wp_json_encode( $body );
-
-	$options = [
-		'body'        => $body,
-		'headers'     => [ 
-			'Content-Type' => 'application/json',
-			'Authorization' => 'Bearer ' . $token,
-		],
-		'timeout'     => 60,
-		'data_format' => 'body',
-	];
-
-	$MessageApiResponse = wp_remote_post( $endpoint, $options );
-	
-	
-	wp_send_json_success([$MessageApiResponse]);
-
-}
